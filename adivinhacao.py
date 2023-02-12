707f4aa5b748444d8a6b1b0c520573252030e812
Leonardo Lima <leonardolimarj@outlook.com>
2023-02-12 17:31:37 -0300

    Seguindo
    Substituindo variáveis e condição elif

--- adivinhacao.py
+++ adivinhacao.py
@@ -5,12 +5,16 @@ numero_secreto = 45
 
 numero_str = input("Digite o seu palpite")
 numero = int(numero_str)
+acertou=(numero == numero_secreto)
+maior=(numero > numero_secreto)
+menor=(numero < numero_secreto)
 
 
-if numero == numero_secreto:
+if acertou:
     print("Você acertou!")
-else:
-    if numero>numero_secreto:
-        print("Você chutou muito pra cima!")
-    elif numero<numero_secreto:
-        print("Você chutou muito pra baixo!")
+elif maior:
+    print("Você errou, chutou muito pra cima!")
+elif menor:
+    print("Você errou, chutou muito pra baixo!")
+
+print("Fim")
