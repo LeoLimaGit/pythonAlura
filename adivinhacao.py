306ef6c7c752a38f793d40c6854564440fb929a7
Leonardo Lima <leonardolimarj@outlook.com>
2023-02-12 18:05:57 -0300

    Seguindo
    loop while

--- adivinhacao.py
+++ adivinhacao.py
@@ -2,19 +2,24 @@ print("*********************************")
 print("Bem vindo ao jogo da adivinhação!")
 print("*********************************")
 numero_secreto = 45
+tentativas = 5
+rodada = 1
 
-numero_str = input("Digite o seu palpite")
-numero = int(numero_str)
-acertou=(numero == numero_secreto)
-maior=(numero > numero_secreto)
-menor=(numero < numero_secreto)
+while(rodada <= tentativas):
+    print("Tentativa {} de {}".format(rodada, tentativas))
+    numero_str = input("Digite o seu palpite")
+    numero = int(numero_str)
+    acertou=(numero == numero_secreto)
+    maior=(numero > numero_secreto)
+    menor=(numero < numero_secreto)
 
 
-if acertou:
-    print("Você acertou!")
-elif maior:
-    print("Você errou, chutou muito pra cima!")
-elif menor:
-    print("Você errou, chutou muito pra baixo!")
-
+    if acertou:
+        print("Você acertou!")
+        break
+    elif maior:
+        print("Você errou, chutou muito pra cima!")
+    elif menor:
+        print("Você errou, chutou muito pra baixo!")
+    rodada= rodada +1
 print("Fim")
