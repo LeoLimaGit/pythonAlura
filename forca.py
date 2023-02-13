16016579c590e2c23de559a510dd682ae0398c03
Leonardo Lima <leonardolimarj@outlook.com>
2023-02-13 12:52:28 -0300

    Inicio curso 2
    Forca com Python

--- forca.py
+++ forca.py
@@ -5,6 +5,18 @@ def jogar_forca():
     print("*********************************")
     print("***Bem vindo ao jogo da forca!***")
     print("*********************************")
+    palavra_secreta= "banana"
+
+    enforcou = False
+    acertou = True
+
+    while(not enforcou and not acertou):
+        print("Jogando...")
+
+
+
+
+
 
 
     print("Fim do jogo")
