09ffbf36d6935d98e1947e1255f1eb3546adabd8
Leonardo Lima <leonardolimarj@outlook.com>
2023-02-14 17:42:46 -0300

    Seguindo
    Forca funcionando, manipulação de arquivos com python.

--- forca.py
+++ forca.py
@@ -1,15 +1,32 @@
-
+import random
 def jogar_forca():
 
 
     print("*********************************")
     print("***Bem vindo ao jogo da forca!***")
     print("*********************************")
-    palavra_secreta= "python".upper()
+
+
+
+    palavras = []
+
+    with open("palavras.txt") as arquivo:
+        for linha in arquivo:
+            linha = linha.strip()
+            palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0,len(palavras))
+
+    palavra_secreta = palavras[numero].upper()
     letras_acertadas = ["_" for letra in palavra_secreta]
     enforcou = False
     acertou = False
     erros = 0
+
+    print(letras_acertadas)
+
     while(not enforcou and not acertou):
         chute = input("Qual letra? :")
         chute = chute.strip().upper()
@@ -31,6 +48,7 @@ def jogar_forca():
         print(letras_acertadas)
     if ("_" not in letras_acertadas):
         print("Você ganhou!")
+        print(letras_acertadas)
     else:
         print("Você perdeu")
     print("Fim do jogo")
